4d5037ecdfebcf4d6e76d069734d2a8c95314655
Saleem Ahmed <130243199+saleem-ahmed-313@users.noreply.github.com>
2024-10-15 09:43:24 +0530

    index.js
    Document code and removed the unwanted comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//player class 
 class Player {
 
     constructor(name, score) {
@@ -19,9 +20,6 @@ class Player {
 }
 
 
-
-
-
 let player1 = document.getElementById('player1');
 let player2 = document.getElementById('player2');
 let first = document.getElementById('first');
@@ -41,8 +39,7 @@ let playagain = document.getElementById('playagain');
 
 var p1, p2;
 
-
-
+// When Start btn is clicked object are initilize for player with name and moves to next Interface
 startGame.addEventListener('click', () => {
     p1 = new Player(player1.value, 0);
     p2 = new Player(player2.value, 0);
@@ -56,7 +53,7 @@ startGame.addEventListener('click', () => {
 
 });
 
-
+// list of category 
 let category =  {
     "arts_and_literature" : true,
     "geography" : true,
@@ -65,10 +62,11 @@ let category =  {
     "sports_and_leisure" : true 
 };
 
+
+// Dyanmically allocating the option in select 
 let sel = document.createElement('option')
 sel.text  = 'select the option'
 document.getElementById('category').appendChild(sel);
-
 for(let s of Object.keys(category)){
     let option = document.createElement('option');
     option.value = category[s];
@@ -76,49 +74,21 @@ for(let s of Object.keys(category)){
     document.getElementById('category').appendChild(option);
 }
 
-
+// When the select change on change event is triggered and quiz game start 
 let categorylist = document.getElementById('category');
-
 categorylist.addEventListener('change', () => {
-
+    
     var selectedCategory = categorylist.options[categorylist.selectedIndex].text;
-
-    //let p = document.createElement('p');
-    ////p.innerHTML = selectedCategory;
-    //document.getElementById('catShow').appendChild(p);
-    categorylist.options[categorylist.selectedIndex].disabled = true;
-    category[selectedCategory] = false;
-    //console.log(category);
+    
+    categorylist.options[categorylist.selectedIndex].disabled = true; // disabling the select category so that it can't be reuse again
+    category[selectedCategory] = false; // setting the category value as false ( it will be useful to end the game when all the category is selected 
     cat.style.display = 'none';
     sec.style.display = 'none';
-    game(selectedCategory, 2, 2, 2);
+    game(selectedCategory, 2, 2, 2); // calling the game function
     third.style.display = 'block';
-    //console.log(p1.getScore());
-    //console.log(p2.getScore());
-    //isCategoryEmpty();
-    //console.log(category);
-
-
 });
 
 end.addEventListener('click', () => {
-   /* third.style.display = 'none';
-    end.style.display = 'none';
-    fourth.style.display = 'block';
-    cat.style.display = 'none';
-    sec.style.display = 'none';
-
-    score1.innerHTML = `The score of ${p1.getName()} : ${p1.getScore()}`;
-    score2.innerHTML =  `The score of ${p2.getName()} : ${p2.getScore()}`;
-    if(p1.getScore() > p2.getScore()){
-        document.getElementById('win').innerHTML = `${p1.getName()} is the winner`;
-    }else if(p1.getScore() < p2.getScore()){
-        document.getElementById('win').innerHTML = `${p2.getName()} is the winner`;
-    }
-    else{
-        document.getElementById('win').innerHTML = `It's a tie`;
-    }
-        */
        endFunction();
 });
 
@@ -132,16 +102,14 @@ playagain.addEventListener('click', () => {
             categorylist.options[i].removeAttribute('disabled');
             category[categorylist.options[i].text] = true;
         }
-        //categorylist.options[option].removeAttribute('disabled');
-        //category[option] = true;
-        //console.log(category);
+        
     });
 
 
-const game = async(selectedCategory, easy, medium, hard) => {
+const game = async(selectedCategory, easy, medium, hard) => { // async function 
 
 
-    while(easy != 0) {
+    while(easy != 0) {     // to display two easy question first
 
         if(easy == 2){
             playerID.innerHTML = `Player Turn : ${p1.getName()}-------------------------          Level : Easy`;
@@ -149,7 +117,7 @@ const game = async(selectedCategory, easy, medium, hard) => {
             playerID.innerHTML = `Player Turn : ${p2.getName()}--------------------------          Level : Easy`; ;
         }
 
-        await displayQuestion(selectedCategory, 'easy');
+        await displayQuestion(selectedCategory, 'easy');   // calling the display function 
         easy--;
 
     }
@@ -181,50 +149,45 @@ const game = async(selectedCategory, easy, medium, hard) => {
     sec.style.display = 'block';
     third.style.display = 'none';
     end.style.display = 'block';
-    isCategoryEmpty();
+    isCategoryEmpty(); // calling the checking functon if all the category is selected , then game is ended 
 
 }
 const displayQuestion = async (selectedCategory, difficulty) => {
     try {
+        // fetch the data from the API
         const response = await fetch(`https://the-trivia-api.com/v2/questions?categories=${selectedCategory}&difficulties=${difficulty}&limit=1`);
+        // Converting in json data format 
         const data = await response.json();
 
-        
+        // setting the question 
         question.innerHTML = data[0].question.text;
 
-        
+        // displaying the options 
         let answers = data[0].incorrectAnswers;
         answers.push(data[0].correctAnswer);
         answers.sort();
-
         btndiv.innerHTML = '';
-
-        
-        
         answers.forEach(answer => {
             const option = document.createElement('button');
             option.innerHTML = answer;
-
-            
-            option.addEventListener('click', () => {
-                answerSelect(option, data[0].correctAnswer,difficulty);
+        
+            option.addEventListener('click', () => {              // Handling the click event if the option is clicked 
+                answerSelect(option, data[0].correctAnswer,difficulty);      // function here update the score if the answer is correct
             });
 
             btndiv.appendChild(option);
         });
-        await waitForUserSelection();
+        await waitForUserSelection();         // Making the javascript wait till the user response 
 
-    } catch (error) {
+    } catch (error) {         // handing the error
         console.error(error);
     }
 };
 
+// this is function makes javascript wait till the user response 
 const waitForUserSelection = () => {
     return new Promise(resolve => {
-        
         const buttons = btndiv.querySelectorAll('button');
-
-        
         buttons.forEach(button => {
             button.addEventListener('click', () => {
                 resolve();
@@ -233,10 +196,10 @@ const waitForUserSelection = () => {
     });
 };
 
+// function checks if the select option is correct then corresponding score update to user who clicked 
 const answerSelect= (selectedOption, correctAnswer,difficulty) => {
     const selectedAnswer = selectedOption.innerHTML;
 
-    
     if (selectedAnswer === correctAnswer) {
         if (playerID.innerHTML === p1.getName()) {
             if(difficulty === 'easy'){
@@ -257,12 +220,11 @@ const answerSelect= (selectedOption, correctAnswer,difficulty) => {
         }
     }
 
-    
     btndiv.innerHTML = '';
-    //displayQuestion(selectedCategory, difficulty);
 };
 
 
+// this function checks if all the category is selected then end function is invoked 
 function isCategoryEmpty(){
     
     let count = 0;
@@ -275,12 +237,13 @@ function isCategoryEmpty(){
     }
 
     if(count == 5){
+        // then function display the result 
         endFunction();
     }
 }
 
 
-
+// this function display the result 
 const endFunction = () => {
     third.style.display = 'none';
     end.style.display = 'none';
@@ -290,6 +253,7 @@ const endFunction = () => {
 
     score1.innerHTML = `The score of ${p1.getName()} : ${p1.getScore()}`;
     score2.innerHTML =  `The score of ${p2.getName()} : ${p2.getScore()}`;
+    // Player with high score is declared as the winner 
     if(p1.getScore() > p2.getScore()){
         document.getElementById('win').innerHTML = `${p1.getName()} is the winner`;
     }else if(p1.getScore() < p2.getScore()){
